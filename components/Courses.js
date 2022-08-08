79dbc28379961d48c0a65579110932ab10dc40ad
hamza@ecosense-enviro.com <hamza@ecosense-enviro.com>
2022-08-08 07:13:54 -0700

    removed robotics and increased prices

--- components/Courses.js
+++ components/Courses.js
@@ -5,7 +5,6 @@ import Link from "next/link";
 
 function Courses() {
   const web = "/web.jpg";
-  const robo = "/robo.jpg";
   const pro = "/pro.jpg";
   const graphic = "/graphic.jpg";
   return (
@@ -21,7 +20,7 @@ function Courses() {
         </div>
         <div className="flex items-center justify-between w-full">
           <div className="text-gray-800 font-semibold">Web Development</div>
-          <div className="text-orange-600 font-medium text-sm">NGN 10,000</div>
+          <div className="text-orange-600 font-medium text-sm">NGN 55,000</div>
         </div>
         <div className="flex items-center justify-between w-full">
           <div className="flex items-center justify-start">
@@ -30,7 +29,7 @@ function Courses() {
           </div>
           <Link href="/courses/web">
             <button className="text-sm bg-orange-500 px-3 py-2 rounded-md text-white hover:bg-orange-400">
-              View Course Content
+              View Content
             </button>
           </Link>
         </div>
@@ -47,7 +46,7 @@ function Courses() {
         </div>
         <div className="flex items-center justify-between w-full">
           <div className="text-gray-800 font-semibold">Graphic Design</div>
-          <div className="text-orange-600 font-medium text-sm">NGN 10,000</div>
+          <div className="text-orange-600 font-medium text-sm">NGN 45,000</div>
         </div>
         <div className="flex items-center justify-between w-full">
           <div className="flex items-center justify-start">
@@ -56,38 +55,12 @@ function Courses() {
           </div>
           <Link href="/courses/design">
             <button className="text-sm bg-orange-500 px-3 py-2 rounded-md text-white hover:bg-orange-400">
-              View Course Content
+              View Content
             </button>
           </Link>
         </div>
       </div>
-      {/*  */}
-      <div className="flex flex-col space-y-2 items-center shadow-sm bg-orange-50 w-full p-4">
-        <div className="relative h-60 w-full">
-          <Image
-            src={robo}
-            objectFit="cover"
-            className="rounded-md"
-            layout="fill"
-          />
-        </div>
-        <div className="flex items-center justify-between w-full">
-          <div className="text-gray-800 font-semibold">Robotics</div>
-          <div className="text-orange-600 font-medium text-sm">NGN 15,000</div>
-        </div>
-        <div className="flex items-center justify-between w-full">
-          <div className="flex items-center justify-start">
-            <Stars />
-            <div className="text-sm text-gray-700">(80 votes)</div>
-          </div>
-          <Link href="/courses/web">
-            <button className="text-sm bg-orange-500 px-3 py-2 rounded-md text-white hover:bg-orange-400">
-              View Course Content
-            </button>
-          </Link>
-        </div>
-      </div>
-      {/*  */}
+      
       <div className="flex flex-col space-y-2 items-center shadow-sm bg-orange-50 w-full p-4">
         <div className="relative h-60 w-full">
           <Image
@@ -101,7 +74,7 @@ function Courses() {
           <div className="text-gray-800 font-semibold">
             Introduction to Python Programming
           </div>
-          <div className="text-orange-600 font-medium text-sm">NGN 10,000</div>
+          <div className="text-orange-600 font-medium text-sm">NGN 50,000</div>
         </div>
         <div className="flex items-center justify-between w-full">
           <div className="flex items-center justify-start">
@@ -110,7 +83,7 @@ function Courses() {
           </div>
           <Link href="/courses/python">
             <button className="text-sm bg-orange-500 px-3 py-2 rounded-md text-white hover:bg-orange-400">
-              View Course Content
+              View Content
             </button>
           </Link>
         </div>
